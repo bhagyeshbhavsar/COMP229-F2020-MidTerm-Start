1fe000144eff0681dbc42c98218166ca9ac67a80
bhagyeshbhavsar <bhavsarbhagyesh70@gmail.com>
2022-03-07 02:48:25 -0500

    updated URI

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://bhagyesh:APVpPYYcF5uaX36y@cluster0.ifkc8.mongodb.net/Midtermbooks?retryWrites=true&w=majority"
 };
